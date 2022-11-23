7f694437e7d22628e52012cb2510078f1878f122
unknown <W1ght>
2022-11-24 00:10:17 +0800

    button

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // import './App.css'
 import Login from './pages/Login'
 import Layout from './pages/Layout'
+import { Button, Space } from 'antd';
 
 function App() {
 
   return (
     <BrowserRouter>
       <div className="App">
+        <Button type="primary">Primary Button</Button>
         <Routes>
           <Route path='/' element={<Layout />}></Route>
           <Route path='/login' element={<Login />}></Route>
